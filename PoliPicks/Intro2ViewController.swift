37f9bddeccbb0a2da050c1be74045ffc8447dfc5
gyqi <gyqi28@gmail.com>
2016-03-14 21:00:17 -0700

    Add Settings VC

---
+++ PoliPicks/Intro2ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  Intro2ViewController.swift
+//  PoliPicks
+//
+//  Created by Grace Qi on 3/14/16.
+//  Copyright © 2016 Justin Peng. All rights reserved.
+//
+
+import UIKit
+
+class Intro2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        //super.viewDidLoad()
+        //let digitsButton = DGTAuthenticateButton(authenticationCompletion: { (session, error) in
+            // Inspect session/error objects
+        //})
+        //self.view.addSubview(digitsButton)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pressSignUpButton(sender: UIButton) {
+        //let digits = Digits.sharedInstance()
+        //digits.authenticateWithCompletion { (session, error) in
+            // Inspect session/error objects
+        //}
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
