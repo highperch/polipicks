07c37743403483a82fcac1d961218231700f93f5
highperch <jpeng@box.com>
2016-03-18 17:37:03 -0700

    Added some NSUserDefaults
    Store pick date and picks when submitted at the end.
    
    Still need to migrate some of the code to ViewWillAppear so that it
    will run at the appropriate times.

--- PicksViewController.swift
+++ PicksViewController.swift
@@ -11,30 +11,53 @@ import AFNetworking
 
 class PicksViewController: UIViewController {
     
+    //Store user's picks
     var berniePick: Bool!
     var hillaryPick: Bool!
     var donaldPick: Bool!
     var johnPick: Bool!
     var marcoPick: Bool!
     
+    //Store the user's pick date
+    var pickDate: NSDate!
+    
+    //pickIndex to iterate over each possible candidate
     var pickIndex: Int!
+    //Store original center for animations
     var cardViewOriginalCenter: CGPoint!
     
+    //Storing the last updated date to compare to the user's last guesses
     var lastUpdated: Int!
     
+    //Arrays for picks and images/names/scores
     var images = ["bernie-sanders", "hillary-clinton", "donald-trump", "john-kasich", "marco-rubio"]
     var names = ["Bernie Sanders", "Hillary Clinton", "Donald Trump", "John Kasich", "Marco Rubio"]
     var picks: [Bool]!
     
+    //Stored value for current scores
+    var berniePerformance: Int!
+    var hillaryPerformance: Int!
+    var donaldPerformance: Int!
+    var johnPerformance: Int!
+    var marcoPerformance: Int!
+    
+    //NSUserDefaults initialization
+    var defaults = NSUserDefaults()
+    
     var responseData: [NSDictionary] = []
     
+    //Outlets for views (make pick, closed, score)
+    @IBOutlet weak var picksClosedView: UIView!
+    @IBOutlet weak var makeAPickView: UIView!
+    
+    //Outlets for Make a Pick elements
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var candidateImage: UIImageView!
     @IBOutlet weak var candidateName: UILabel!
     @IBOutlet var cardViewPanGestureRecognizer: UIPanGestureRecognizer!
     
     func didChooseUp() {
-        if pickIndex > 4 {
+        if pickIndex < 5 {
         picks[pickIndex] = true
         print(pickIndex)
         UIView.animateWithDuration(0.4, animations: { () -> Void in
@@ -45,7 +68,7 @@ class PicksViewController: UIViewController {
     }
     
     func didChooseDown() {
-        if pickIndex > 4 {
+        if pickIndex < 5 {
         picks[pickIndex] = false
         print(pickIndex)
         UIView.animateWithDuration(0.4, animations: { () -> Void in
@@ -78,6 +101,29 @@ class PicksViewController: UIViewController {
         }
     }
     
+    func picksCompleted() {
+        pickDate = NSDate()
+        defaults.setValue(pickDate, forKey: "pickDate")
+        defaults.setValue(berniePick, forKey: "berniePick")
+        defaults.setValue(hillaryPick, forKey: "hillaryPick")
+        defaults.setValue(donaldPick, forKey: "donaldPick")
+        defaults.setValue(johnPick, forKey: "johnPick")
+        defaults.setValue(marcoPick, forKey: "marcoPick")
+        defaults.synchronize()
+        
+        makeAPickView.alpha = 0
+        picksClosedView.alpha = 1
+    }
+    
+    func calculateScores() {
+        
+    }
+    
+    func resetPicks() {
+        pickIndex = 0
+        
+    }
+    
     @IBAction func didPanCard(sender: UIPanGestureRecognizer) {
         var translation = sender.translationInView(view).y
         var velocity = sender.velocityInView(view).y
@@ -86,8 +132,10 @@ class PicksViewController: UIViewController {
             
         } else if sender.state == UIGestureRecognizerState.Changed {
             cardView.center.y = cardViewOriginalCenter.y + translation
+            print(translation)
             
         } else if sender.state == UIGestureRecognizerState.Ended {
+            print("ended")
             //If card was moving down past a certain point/speed
             if velocity > 0 || translation > 100 {
                 //Consider it a choice for down
@@ -109,32 +157,48 @@ class PicksViewController: UIViewController {
     }
     
     override func viewWillAppear(animated: Bool) {
-        if pickIndex > 5 {
+        if pickIndex > 4 {
             
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Stored value for current poll's scores
+        var bernieCurrent: Int!
+        var hillaryCurrent: Int!
+        var donaldCurrent: Int!
+        var johnCurrent: Int!
+        var marcoCurrent: Int!
+        
+        //Stored value for last poll's scores
+        var berniePast: Int!
+        var hillaryPast: Int!
+        var donaldPast: Int!
+        var johnPast: Int!
+        var marcoPast: Int!
+        
         //cardView.layer.shadowOffset = CGSizeMake(5,1)
         cardView.layer.shadowRadius = 1
         cardView.layer.shadowOpacity = 1
         cardViewOriginalCenter = cardView.center
         
+        //Setting the first candidate (Bernie)'s assets
         pickIndex = 0
         candidateImage.image = UIImage(named: images[pickIndex])
         candidateName.text = names[pickIndex]
         
+        //Setting defaults to up (not sure why this is needed for the app not to crash
         berniePick = true
         hillaryPick = true
         donaldPick = true
         johnPick = true
         marcoPick = true
         
+        //Populating picks
         picks = [berniePick, hillaryPick, donaldPick, johnPick, marcoPick]
-        // Do any additional setup after loading the view.
         
-        //AFNetworking
+        //AFNetworking Setup
         let url = NSURL(string:"http://elections.huffingtonpost.com/pollster/api/charts/2016-national-gop-primary.json")
         let request = NSURLRequest(URL: url!)
         let session = NSURLSession(
@@ -143,6 +207,7 @@ class PicksViewController: UIViewController {
             delegateQueue:NSOperationQueue.mainQueue()
         )
         
+        //AFNetworking Handling
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (data, response, error) in
                 if let data = data {
